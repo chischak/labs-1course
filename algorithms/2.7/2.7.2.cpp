53a289cb60614302a24048c4045327d23381ffce
chischak <117519491+chischak@users.noreply.github.com>
2022-12-10 21:49:11 +0500

    Add files via upload

---
+++ algorithms/2.7/2.7.2.cpp
@@ -0,0 +1,28 @@
+//2.7 Напечатать квадрат из N звездочек. Заполненный квадрат со стороной N.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;              
+    cin >> n;
+    if (n < 2)
+    {
+        cout << "трай эгеин";
+    }
+    else
+    {
+        for (int i = 0; i < n; i++)
+        {
+            for (int a = 0; a < n; a++)
+            {
+               cout << '*' << ' '; 
+            }
+            cout << endl;
+         }
+    }
+    return 0;
+}
---
+++ algorithms/2.7/2.7.3.cpp
@@ -0,0 +1,35 @@
+//2.7 Напечатать квадрат из N звездочек. Пустой квадрат со стороной N. В каждом из вариантов задать требования к минимальному N.
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;              
+    cin >> n;
+    if (n < 3)
+    {
+        cout << "трай эгеин";
+    }
+    else
+    {
+         for(int i = 0; i < n; i++)
+         {
+             for(int a = 0; a < n; a++)
+             {
+                 if(i == 0 or a == 0 or i == n - 1 or a == n - 1)
+                 {
+                     cout << '*' << ' ';
+                 }
+                 else
+                 {
+                     cout << ' ' << ' ';
+                 }
+             } cout << endl;
+             
+         }
+    }
+    return 0;
+}
