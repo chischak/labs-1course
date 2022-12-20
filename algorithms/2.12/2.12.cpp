2acb3fb835a338bf76bd124d79350ccf173019c3
chischak <117519491+chischak@users.noreply.github.com>
2022-12-20 15:47:44 +0500

    Add files via upload

---
+++ algorithms/2.12/2.12.cpp
@@ -0,0 +1,34 @@
+//2.12 Последовательность из N положительных чисел вводится с клавиатуры. Найти сумму максимального и минимального элементов последовательности.
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n, tmp, max, min;
+
+cout << "Введите длину последовательности:" << endl;
+cin >> n;
+
+cout << "Введите первое число:" << endl;
+cin >> tmp;
+max = tmp;
+min = tmp;
+
+for (int i = 2; i <= n; i++)
+{
+    cout << "Введите следующее число:" << endl;
+    cin >> tmp;
+    if (tmp > max)
+    {
+        max = tmp;
+    }
+    else if (tmp < min)
+    {
+        min = tmp;
+    }
+}
+cout << "Сумма максимального и минимального элементов: " << (max+min) << endl;
+    return 0;
+}
